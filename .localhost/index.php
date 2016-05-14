0ecf1f6f48be5a3bc792cb9f786dc7632dfc9f51
Anderson Camargo <andergtk@gmail.com>
2016-05-14 04:41:20 -0300

    Add feature to display subdirectories looking good. Fix #2

--- .localhost/index.php
+++ .localhost/index.php
@@ -1,137 +1,117 @@
 <?php
 
-/** Change it if needed */
-define( 'HOME_URL',     'http://' . $_SERVER['HTTP_HOST'] );
-define( 'APACHE_DIR',   '/etc/apache2' );
-define( 'PROJECTS_DIR', '../' );
+require_once( 'config.php' );
 
-/** Add here the name of the virtual hosts files to be ignored */
-$sites_ignore    = array(
-	'000-default.conf',
-	'default-ssl.conf'
-);
-
-$projects        = array();
+$files           = array();
+$directories     = array();
 $sites_available = array();
 $sites_enabled   = array();
+
+/** Support for subdirectories */
+if ( empty( $_SERVER['REQUEST_URI'] ) )
+	$subdir = '/';
+else
+	$subdir = trim( $_SERVER['REQUEST_URI'] );
 
 /** To ignore hidden things */
 $pattern = '/^\./';
 
-/** Stores the folder name of the projects */
-$dir = opendir( PROJECTS_DIR );
-while ( $folder_name = readdir( $dir ) )
-	if ( ! preg_match( $pattern, $folder_name ) )
-		$projects[] = $folder_name;
-closedir( $dir );
+/** Stores the name of the files */
+foreach ( glob( PROJECTS_DIR . $subdir . '/*' ) as $file_name )
+	if ( ! in_array( basename( $file_name ), $ignore ) ) {
+		$files[] = basename( $file_name );
+		if ( is_dir( $file_name ) )
+			$directories[] = basename( $file_name ) ;
+	}
 
 /** Stores the sites available */
-$dir = opendir( APACHE_DIR . '/sites-available' );
-while ( $conf_file = readdir( $dir ) )
-	if ( ! preg_match( $pattern, $conf_file ) && ! in_array( $conf_file, $sites_ignore ) )
-		$sites_available[] = $conf_file;
-closedir( $dir );
+foreach ( glob( APACHE_DIR . '/sites-available/*' ) as $conf_file )
+	if ( ! in_array( basename( $conf_file ), $ignore ) )
+		$sites_available[] = basename( $conf_file );
 
 /** Stores the sites enabled */
-$dir = opendir( APACHE_DIR . '/sites-enabled' );
-while ( $conf_file = readdir( $dir ) )
-	if ( ! preg_match( $pattern, $conf_file ) )
-		$sites_enabled[] = $conf_file;
-closedir( $dir );
+foreach( glob( APACHE_DIR . '/sites-enabled/*' ) as $conf_file )
+	if ( ! in_array( basename( $conf_file ), $ignore ) )
+		$sites_enabled[] = basename( $conf_file );
+
+require_once( 'header.php' );
 
 ?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Localhost</title>
-		<link rel="shortcut icon" type="image/x-icon" href="<?php echo HOME_URL . '/.localhost/favicon.ico'; ?>">
-		<link rel="stylesheet" type="text/css" href="<?php echo HOME_URL . '/.localhost/style.css'; ?>">
-	</head>
-	<body>
-		<nav class="navbar">
-			<div class="container">
-				<div class="branding">
-					<h1><a href="<?php echo HOME_URL; ?>">Localhost</a></h1>
-				</div><!-- .branding -->
-				<ul class="menu">
-					<li><a href="<?php echo HOME_URL . '/phpmyadmin'; ?>">PhpMyAdmin</a></li>
-					<li>
-						<a class="github-link" href="https://github.com/andergtk/localhost-looking-good" target="_blank">
-							<img src="<?php echo HOME_URL . '/.localhost/github.png'; ?>" title="See this project on GitHub" alt="GitHub">
-						</a>
-					</li>
-				</ul><!-- .menu -->
-			</div><!-- .container -->
-		</nav><!-- .navbar -->
+<div class="column">
+	<div class="panel">
+		<div class="panel-heading">
+			<?php if ( '/' !== $subdir ) : ?>
+				<a class="btn-back" href="<?php echo HOME_URL . dirname( $subdir ); ?>"></a>
+				<h3><?php echo ucfirst( basename( $subdir ) ); ?></h3>
+			<?php else : ?>
+				<h3>Projects</h3>
+			<?php endif; ?>
+		</div><!-- .panel-heading -->
+		<div class="panel-body">
+			<div class="list-group">
+				<?php if ( empty( $files ) ) : ?>
+					<p class="list-group-item">No Files</p>
+				<?php else : ?>
+					<?php foreach ( $files as $file ) : ?>
+						<?php if ( in_array( $file, $directories ) ) : ?>
+							<a class="list-group-item is-dir" href="<?php echo HOME_URL . $subdir . $file; ?>">
+								<h4><?php echo $file; ?></h4>
+							</a>
+						<?php else : ?>
+							<a class="list-group-item is-file" href="<?php echo HOME_URL . $subdir . $file; ?>">
+								<h4><?php echo $file; ?></h4>
+							</a>
+						<?php endif; ?>
+					<?php endforeach; ?>
+				<?php endif; ?>
+			</div><!-- .list-group -->
+		</div><!-- .panel-body -->
+	</div><!-- .panel -->
+</div><!-- .column -->
 
-		<div class="container">
-			<div class="column">
-				<div class="panel">
-					<div class="panel-heading">
-						<h3>Projects</h3>
-					</div><!-- .panel-heading -->
-					<div class="panel-body">
-						<div class="list-group">
-							<?php if ( empty( $projects ) ) : ?>
-								<p class="list-group-item">No Projects</p>
-							<?php else : ?>
-								<?php foreach ( $projects as $project ) : ?>
-									<a class="list-group-item" href="<?php echo HOME_URL . "/{$project}"; ?>">
-										<h4><?php echo $project; ?></h4>
-									</a>
-								<?php endforeach; ?>
-							<?php endif; ?>
-						</div><!-- .list-group -->
-					</div><!-- .panel-body -->
-				</div><!-- .panel -->
-			</div><!-- .column -->
+<div class="column">
+	<div class="panel">
+		<div class="panel-heading">
+			<h3>Virtual Hosts</h3>
+		</div><!-- .panel-heading -->
+		<div class="panel-body">
+			<div class="list-group">
+				<?php if ( empty( $sites_available ) ) : ?>
+					<p class="list-group-item">No Virtual Hosts</p>
+				<?php else : ?>
+					<?php foreach ( $sites_available as $site ) : ?>
+						<?php
 
-			<div class="column">
-				<div class="panel">
-					<div class="panel-heading">
-						<h3>Virtual Hosts</h3>
-					</div><!-- .panel-heading -->
-					<div class="panel-body">
-						<div class="list-group">
-							<?php if ( empty( $sites_available ) ) : ?>
-								<p class="list-group-item">No Virtual Hosts</p>
-							<?php else : ?>
-								<?php foreach ( $sites_available as $site ) : ?>
-									<?php
+						/** Reads entire file into an array */
+						$vh = file( APACHE_DIR . "/sites-available/{$site}" );
+						foreach ( $vh as $line ) {
 
-									/** Reads entire file into an array */
-									$file = file( APACHE_DIR . "/sites-available/{$site}" );
-									foreach ( $file as $line ) {
+							/** Search for the line with the ServerName */
+							preg_match( '/ServerName\s+(.*)\n/', $line, $server_name);
+							if ( isset( $server_name[1] ) ) {
+								$url = "http://{$server_name[1]}";
+								break;
+							}
+						}
 
-										/** Search for the line with the ServerName */
-										preg_match( '/ServerName\s+(.*)\n/', $line, $server_name);
-										if ( isset( $server_name[1] ) ) {
-											$url = "http://{$server_name[1]}";
-											break;
-										}
-									}
+						/** If there's no ServerName, set a common URL */
+						if ( ! isset( $url ) )
+							$url = HOME_URL . "/{$site}";
 
-									/** If there's no ServerName, set a common URL */
-									if ( ! isset( $url ) )
-										$url = HOME_URL . "/{$site}";
+						/** Verify if the virtual host configuration is enabled */
+						$label_type = ( in_array( $site, $sites_enabled ) ) ? 'success' : 'danger';
 
-									/** Verify if the virtual host configuration is enabled */
-									$label_type = ( in_array( $site, $sites_enabled ) ) ? 'success' : 'danger';
+						?>
+						<a class="list-group-item" href="<?php echo $url; ?>">
+							<h4><?php echo $site; ?></h4>
+							<span class="label <?php echo "label-{$label_type}"; ?>"></span>
+							<p><?php echo $url; ?></p>
+						</a><!-- .list-group-item -->
+					<?php endforeach; ?>
+				<?php endif; ?>
+			</div><!-- .list-group -->
+		</div><!-- .panel-body -->
+	</div><!-- .panel -->
+</div><!-- .column -->
 
-									?>
-									<a class="list-group-item" href="<?php echo $url; ?>">
-										<h4><?php echo $site; ?></h4>
-										<span class="label <?php echo "label-{$label_type}"; ?>"></span>
-										<p><?php echo $url; ?></p>
-									</a><!-- .list-group-item -->
-								<?php endforeach; ?>
-							<?php endif; ?>
-						</div><!-- .list-group -->
-					</div><!-- .panel-body -->
-				</div><!-- .panel -->
-			</div><!-- .column -->
-		</div><!-- .container -->
-	</body>
-</html>
+<?php require_once( 'footer.php' ); ?>
