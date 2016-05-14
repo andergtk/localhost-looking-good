fa74e8843652eec72cc4b4ead919b8dcb86870f9
Anderson Camargo <andergtk@gmail.com>
2016-05-14 07:49:12 -0300

    Add feature to display error messages looking good. Fix #1

--- .localhost/error.php
+++ .localhost/error.php
@@ -3,11 +3,34 @@
 require_once( 'config.php' );
 
 $error = array(
-	404 => 'Not found'
+	400 => 'Bad Request',
+	401 => 'Unauthorized',
+	403 => 'Forbidden',
+	404 => 'Not Found',
+	405 => 'Method Not Allowed',
+	407 => 'Proxy Authentication Required',
+	408 => 'Request Timeout',
+	414 => 'URI Too Long',
+	429 => 'Too Many Requests',
+	500 => 'Internal Server Error',
+	502 => 'Bad Gateway',
+	503 => 'Service Unavailable',
+	507 => 'Insufficient Storage',
+	508 => 'Loop Detected',
+	444 => 'No Response',
+	499 => 'Client Closed Request'
 );
+
+$status  = $_SERVER['REDIRECT_STATUS'];
+$title   = $error[ $status ];
 
 require_once( 'header.php' );
 
-echo '<h1>ERROR PAGE</h1>';
+?>
 
-require_once( 'footer.php' );
+<div class="error-page">
+	<span class="error-status"><?php echo $status; ?></span>
+	<span class="error-title"><?php echo $title; ?></span>
+</div><!-- .error-page -->
+
+<?php require_once( 'footer.php' ); ?>
