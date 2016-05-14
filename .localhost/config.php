551f0a94bca7606934e52ff9fff193c4ed1e0171
Anderson Camargo <andergtk@gmail.com>
2016-05-14 03:49:03 -0300

    Add configurations file

---
+++ .localhost/config.php
@@ -0,0 +1,14 @@
+<?php
+
+/** Change it if needed */
+define( 'HOME_URL',     'http://' . $_SERVER['HTTP_HOST'] );
+define( 'APACHE_DIR',   '/etc/apache2' );
+define( 'PROJECTS_DIR', '..' );
+
+/** Add here the name of the virtual hosts files to be ignored */
+$ignore    = array(
+	'000-default.conf',
+	'default-ssl.conf',
+	'LICENSE',
+	'README.md'
+);
